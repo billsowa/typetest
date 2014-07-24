07b1124711b40a34a75e29c3b84853d6c2945031
Bill Sowa <billsowa@gmail.com>
2014-07-23 22:06:29 -0400

    update app.js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,32 @@
 (function ($, window, undefined) {
 
-    $.getJSON("data/words.json", function(data) {
-        processData(data);
-    });
+    
+        var words = [ "a", "about", "above", "across", "act", "active", "activity", "add", "afraid", "after", "again", "age", "ago", "agree", "air", "all", "alone", "along", "already", "always", "am", "amount", "an", "and", "angry", "another", "answer", "any", "anyone", "anything", "anytime", "appear", "apple", "are", "area", "arm", "army", "around", "arrive", "art", "as", "ask", "at", "attack", "aunt", "autumn", "away", "baby", "back", "bad", "bag", "ball", "bank", "base", "basket", "bath", "be", "bean", "bear", "beautiful", "bed", "bedroom", "beer", "behave", "before", "begin", "behind", "bell", "below", "besides", "best", "better", "between", "big", "bird", "birth", "birthday", "bit", "bite", "black", "bleed", "block", "blood", "blow", "blue", "board", "boat", "body", "boil", "bone", "book", "border", "born", "borrow", "both", "bottle", "bottom", "bowl", "box", "boy", "branch", "brave", "bread", "break", "breakfast", "breathe", "bridge", "bright", "bring", "brother", "brown", "brush", "build", "burn", "business", "bus", "busy", "but", "buy", "by", "c", "cake", "call", "can", "candle", "cap", "car", "card", "care", "careful", "careless", "carry", "case", "cat", "catch", "central", "century", "certain", "chair", "chance", "change", "chase", "cheap", "cheese", "chicken", "child", "children", "chocolate", "choice", "choose", "circle", "city", "class", "clever", "clean", "clear", "climb", "clock", "cloth", "clothes", "cloud", "cloudy", "close", "coffee", "coat", "coin", "cold", "collect", "colour", "comb", "comfortable", "common", "compare", "come", "complete", "computer", "condition", "continue", "control", "cook", "cool", "copper", "corn", "corner", "correct", "cost", "contain", "count", "country", "course", "cover", "crash", "cross", "cry", "cup", "cupboard", "cut"];
+        var arr = [];
+        var randomLetter;
 
-    function processData(data) {
+    // $.getJSON("data/words.json", function(data) {
+    //     processData(data);
+    // });
 
-        console.log(data.words);
+    // function processData(data) {
 
-        var randomLetter = data[Math.floor(Math.random() * data.length)];
+    //     console.log(data.words);
+
+    //     var randomLetter = data[Math.floor(Math.random() * data.length)];
         
-        console.log(randomLetter);
+    //     console.log(randomLetter);
 
-        for (var i = 0; i <= data.length; i++) {
-            console.log(i);
-        }
+    //     for (var i = 0; i <= data.length; i++) {
+    //         console.log(i);
+    //     }
+    // }
+
+    for ( var i = 0; i <= 200; i++ ) {
+        randomLetter = words[Math.floor(Math.random() * words.length)];
+        arr.push(randomLetter);    
     }
+
+    console.log(arr);
 
 })(jQuery, window);
